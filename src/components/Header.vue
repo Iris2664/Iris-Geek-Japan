ec70390f4a0d86c598fc0972d530b657dea85f33
SHINONOME Ayame <137654183+Iris2664@users.noreply.github.com>
2024-10-03 00:43:11 +0900

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <header class="px-14 py-8 shadow-sm">
+        <h1 class="text-xl font-semibold">Iris Geek Japan</h1>
+    </header>
+</template>
---
+++ src/components/Main.vue
@@ -0,0 +1,61 @@
+<template>
+    <main class="container mx-auto px-8 py-10">
+        <div>
+            <h2 class="py-4 text-7xl font-semibold text-purple-700">Iris Geek Japan</h2>
+            <p class="py-3 max-w-md text-xl">
+                低レイヤーから高レイヤーなど各種情報通信技術に関する技術者・研究者コミュニティ
+            </p>
+            <div class="mt-8 mb-6">
+                <button class="bg-gray-200 hover:bg-gray-400 text-lg px-5 py-3 rounded-3xl shadow">
+                    <a href="https://x.com/IrisGeekJapan" target="_blank" rel="noopener noreferrer">
+                        X(Twitter)
+                    </a>
+                </button>
+            </div>
+        </div>
+        <div class="flex flex-row mt-12 mb-14">
+            <div class="flex-1 mx-3 p-7 bg-purple-50 rounded-xl shadow-md">
+                <h4 class="text-lg font-semibold mb-2">技術者も研究者もわいわい！</h4>
+                <p>本コミュニティは，低レイヤーから高レイヤーなど各種情報通信技術に関する技術者・研究者コミュニティです．</p>
+            </div>
+            <div class="flex-1 mx-3 p-7 bg-purple-50 rounded-xl shadow-md">
+                <h4 class="text-lg font-semibold mb-2">イベントでわいわい！</h4>
+                <a class="border-b border-gray-300 hover:text-red-400" href="http://" target="_blank" rel="noopener noreferrer">connpass</a> 
+                にてイベント情報をチェックして参加しよう！
+            </div>
+            <div class="flex-1 mx-3 p-7 bg-purple-50 rounded-xl shadow-md">
+                <h4 class="text-lg font-semibold mb-2">IT系コミュニティ</h4>
+                <p>
+                    <a class="border-b border-gray-300 hover:text-red-400" href="https://x.com/share?hashtags=IrisGeekJapan" target="_blank" rel="noopener noreferrer">#IrisGeekJapan</a>
+                </p>
+            </div>
+        </div>
+        <div class="bg-gray-50 px-12 py-6 shadow-md">
+            <h2 class="text-3xl py-2 border-b border-gray-400">More about Iris Geek Japan</h2>
+            <div class="pt-6">
+                <h3 class="text-2xl">What is Iris Geek Japan?</h3>
+                <p class="py-3 text-base">
+                    『Iris Geek Japan』とは, Virtual YouTuberの東雲絢芽によって主宰される情報通信技術に関する業務に従事してる人や情報通信系を専門として研究をしてる人等がわいわいお互いの専門分野について教え合って交流を図るコミュニティ・勉強会です．
+                </p>
+            </div>
+            <div class="py-2">
+                <h3 class="text-2xl">What information and communication technologies are acceptable?</h3>
+                <p class="py-3 text-base">
+                    本当に情報通信技術に関することであれば低レイヤー・高レイヤーやテクノロジーからマネジメントでも大歓迎です．
+                </p>
+            </div>
+            <div class="py-2">
+                <h3 class="text-2xl">What are the main areas for study groups and LT meetings in real space?</h3>
+                <p class="py-3 text-base">
+                    全国各地で実施したいですが，いまのところは東京都，神奈川県，千葉県，埼玉県を中心にリアル会場イベントを行う予定です．
+                </p>
+            </div>
+            <div class="py-2">
+                <h3 class="text-2xl">Are there any online events?</h3>
+                <p class="py-3 text-base">
+                    はい．各地の方々にご参加いただけるようにオンラインイベントも拡充していこうと思います，
+                </p>
+            </div>
+        </div>
+    </main>
+</template>
