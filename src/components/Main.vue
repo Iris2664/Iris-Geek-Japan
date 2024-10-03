a88fd52a503750e394c6f0f69e4ea3be0f42dbfb
SHINONOME Ayame <137654183+Iris2664@users.noreply.github.com>
2024-10-03 18:56:26 +0900

    update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,11 +6,17 @@
                 低レイヤーから高レイヤーなど各種情報通信技術に関する技術者・研究者コミュニティ
             </p>
             <div class="mt-8 mb-6">
-                <button class="bg-gray-200 hover:bg-gray-400 text-lg px-5 py-3 rounded-3xl shadow">
-                    <a href="https://x.com/IrisGeekJapan" target="_blank" rel="noopener noreferrer">
-                        X(Twitter)
-                    </a>
-                </button>
+                <a class="mr-4 bg-gray-200 hover:bg-gray-400 text-lg px-5 py-3 rounded-3xl shadow"
+                    href="https://x.com/IrisGeekJapan" target="_blank" rel="noopener noreferrer">
+                    X(Twitter)
+                </a>
+                <a class="mx-2 bg-gray-200 hover:bg-gray-400 text-lg px-5 py-3 rounded-3xl shadow"
+                    href="https://iris-geek-japan.connpass.com/" target="_blank" rel="noopener noreferrer">
+                    connpass
+                </a>
+                <!--<a  class="mx-4 bg-gray-200 hover:bg-gray-400 text-lg px-5 py-3 rounded-3xl shadow" href="https://x.com/IrisGeekJapan" target="_blank" rel="noopener noreferrer">
+                    X(Twitter)
+                </a>-->
             </div>
         </div>
         <div class="flex flex-row mt-12 mb-14">
@@ -20,14 +26,33 @@
             </div>
             <div class="flex-1 mx-3 p-7 bg-purple-50 rounded-xl shadow-md">
                 <h4 class="text-lg font-semibold mb-2">イベントでわいわい！</h4>
-                <a class="border-b border-gray-300 hover:text-red-400" href="http://" target="_blank" rel="noopener noreferrer">connpass</a> 
+                <a class="border-b border-gray-300 hover:text-red-400" href="https://iris-geek-japan.connpass.com/"
+                    target="_blank" rel="noopener noreferrer">connpass</a>
                 にてイベント情報をチェックして参加しよう！
             </div>
             <div class="flex-1 mx-3 p-7 bg-purple-50 rounded-xl shadow-md">
                 <h4 class="text-lg font-semibold mb-2">IT系コミュニティ</h4>
                 <p>
-                    <a class="border-b border-gray-300 hover:text-red-400" href="https://x.com/share?hashtags=IrisGeekJapan" target="_blank" rel="noopener noreferrer">#IrisGeekJapan</a>
+                    <a class="border-b border-gray-300 hover:text-red-400"
+                        href="https://x.com/share?hashtags=IrisGeekJapan" target="_blank"
+                        rel="noopener noreferrer">#IrisGeekJapan</a>
                 </p>
+            </div>
+        </div>
+        <div class="flex justify-between bg-blue-50 rounded-lg shadow my-12 px-12 py-6">
+            <div>
+                <h2 class="py-3 text-2xl font-semibold">次回の開催予定</h2>
+                <p class="text-lg">イベント名: Iris Geek Part1 in 千葉・船橋</p>
+                <p class="text-lg">開催日: 2025/01/18</p>
+                <p class="text-lg">時間: 15:00 - 17:30</p>
+            </div>
+            <div>
+                <div class="w-full">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5448.9002937127925!2d139.98446978076427!3d35.70320483658846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188078f9cfcb41%3A0x41e25fb2a3af488d!2z6Ii55qmL6aeF!5e0!3m2!1sja!2sjp!4v1727944555252!5m2!1sja!2sjp"
+                        class="w-80 h-80"
+                        width="auto" height="auto" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </div>
         </div>
         <div class="bg-gray-50 px-12 py-6 shadow-md">
@@ -35,7 +60,8 @@
             <div class="pt-6">
                 <h3 class="text-2xl">What is Iris Geek Japan?</h3>
                 <p class="py-3 text-base">
-                    『Iris Geek Japan』とは, Virtual YouTuberの東雲絢芽によって主宰される情報通信技術に関する業務に従事してる人や情報通信系を専門として研究をしてる人等がわいわいお互いの専門分野について教え合って交流を図るコミュニティ・勉強会です．
+                    『Iris Geek Japan』とは, Virtual
+                    YouTuberの東雲絢芽によって主宰される情報通信技術に関する業務に従事してる人や情報通信系を専門として研究をしてる人等がわいわいお互いの専門分野について教え合って交流を図るコミュニティ・勉強会です．
                 </p>
             </div>
             <div class="py-2">
